82fe325e57d8e689666f2945d52ec3b460ec09d5
MrKenobii <anldygc35@windowslive.com>
2022-06-28 15:17:23 +0300

    Injecting values from .properties file & @Bean Annottation

--- src/com/anilduyguc/springdemo/MyFortuneService.java
+++ src/com/anilduyguc/springdemo/MyFortuneService.java
@@ -1,9 +1,11 @@
 package com.anilduyguc.springdemo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
+@PropertySource("classpath:sport.properties")
 public class MyFortuneService implements FortuneService{
     @Value("${foo.fortunes}")
     private String[] fortunes;
--- src/com/anilduyguc/springdemo/SportConfig.java
+++ src/com/anilduyguc/springdemo/SportConfig.java
@@ -1,9 +1,22 @@
 package com.anilduyguc.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@ComponentScan("com.anilduyguc.springdemo")
+@PropertySource("classpath:sport.properties")
+//@ComponentScan("com.anilduyguc.springdemo")
 public class SportConfig {
+
+    @Bean
+    public FortuneService sadFortuneService(){
+        return new SadFortuneService();
+    }
+
+    @Bean
+    public Coach swimCoach(){
+        return new SwimCoach(sadFortuneService());
+    }
 }
